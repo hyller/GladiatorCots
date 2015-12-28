96ab38897828eff39794df77972ece97da7723ec
hyller <haile.jiang@gmail.com>
2015-12-28 10:09:25 +0800

    add tag version

---
+++ tag.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+rem get version
+for /f "usebackq tokens=2*" %%i in (`semver -s -g version ^| findstr /r "^\Input"`) do  set VERSION_STR=%%j
+
+if %VERSION_STR% == "" goto EXIT
+
+git tag GladiatorCots_%VERSION_STR%
+git push origin --tags
+
+:EXIT
+endlocal
+@echo on
